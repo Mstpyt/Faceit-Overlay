2cfce468b54d08333064de0371b78be659a0cab3
Illu <52736876+Mstpyt@users.noreply.github.com>
2021-04-17 16:46:49 +0200

    Update eloOverlay.py

--- eloOverlay.py
+++ eloOverlay.py
@@ -12,6 +12,9 @@ from faceit import faceit_api
 import logging
 
 start_threading = 0
+on_render = 0
+on_progress = 0
+build_is_done = 0
 
 """ -------------------------------------------------------------------------------------------------------------------
                                             THREADING HANDLING
@@ -47,6 +50,8 @@ class Worker:
                 if name:
                     if start_threading == 1:
                         logging.info("Get stats and refresh them")
+                        core.configure_item("##reload_same_line", show=True)
+                        core.configure_item("##reload_image", show=True)
                         self.iElo, self.acEloToday, self.iRank, \
                         self.acResult, self.acScore, self.acKd, \
                         self.acMap, self.iStreak, self.iMatches, \
@@ -64,9 +69,69 @@ class Worker:
                         core.set_value("elodiffmap##", f"{self.acEloDiff}")
                         core.set_value("kills##", f"{self.iKills}")
                         core.set_value("death##", f"{self.iDeath}")
+                        core.configure_item("##reload_same_line", show=False)
+                        core.configure_item("##reload_image", show=False)
                         sleep(60)
             except:
                 self.run()
+
+    def run2(self):
+        global on_render, on_progress
+        logging.warning("run2")
+        try:
+            while 1:
+                if on_render == 1:
+                    if on_progress < 10:
+                        nI = 0.00
+                        for i in range(10):
+                            nI = nI + 0.01
+                            core.configure_item("##ProgressBar", overlay=f"{round(nI*100)}%")
+                            core.set_value("##ProgressBar", nI)
+                            if nI >= 0.10:
+                                break
+                            sleep(0.002)
+                    if 10 >= on_progress < 50:
+                        nI = 0.10
+                        for i in range(10):
+                            nI = nI + 0.01
+                            core.configure_item("##ProgressBar", overlay=f"{round(nI*100)}%")
+                            core.set_value("##ProgressBar", nI)
+                            if nI >= 0.50:
+                                break
+                            sleep(0.09)
+                    if 50 >= on_progress < 80:
+                        core.set_value("##ProgressText", "Getting closer !")
+                        nI = 0.50
+                        for i in range(10):
+                            nI = nI + 0.01
+                            core.configure_item("##ProgressBar", overlay=f"{round(nI*100)}%")
+                            core.set_value("##ProgressBar", nI)
+                            if nI >= 0.80:
+                                break
+                            sleep(0.07)
+                    if 80 >= on_progress < 90:
+                        core.set_value("##ProgressText", "Cmon Faceit !")
+                        nI = 0.80
+                        for i in range(10):
+                            nI = nI + 0.01
+                            core.configure_item("##ProgressBar", overlay=f"{round(nI*100)}%")
+                            core.set_value("##ProgressBar", nI)
+                            if nI >= 0.90:
+                                break
+                            sleep(0.09)
+                    if 90 >= on_progress < 100:
+                        core.set_value("##ProgressText", "..... !")
+                        nI = 0.90
+                        for i in range(10):
+                            nI = nI + 0.01
+                            core.configure_item("##ProgressBar", overlay=f"{round(nI*100)}%")
+                            core.set_value("##ProgressBar", nI)
+                            if nI >= 1.0:
+                                on_render = 0
+                                break
+                            sleep(0.8)
+        except:
+            self.run2()
 
 
 def long_process():
@@ -76,6 +141,12 @@ def long_process():
     w = Worker()
     d = threading.Thread(name='daemon', target=w.run, daemon=True)
     d.start()
+    w = Worker()
+    d = threading.Thread(name='daemon2', target=w.run2, daemon=True)
+    d.start()
+
+
+
 
 
 """ -------------------------------------------------------------------------------------------------------------------
@@ -88,12 +159,17 @@ def add_faceit(iElo, iRank, acEloToday, iStreak, iMatches, iMatchesWon):
     Check all Checkboxes and add the active to the Overlay
     """
     nI = -1
+    skipacEloGoal = 0
+
     list_faceit = sqlite3db.TExecSqlReadMany(DBNAME, """
                                             SELECT * FROM CFG_STATS_FACEIT
                                              """
                                              )
+    acEloGoal = config_functions.get_elo_goal_from_db()
     logging.info("Building Faceit stats")
     core.add_button("\t\tFACEIT STATS\t\t", callback=switch_back_to_menu)
+    core.add_same_line(name="##reload_same_line", show=False)
+    core.add_image("##reload_image", value="resources/ref.png", width=16, height=16,  show=False)
     for i in list_faceit[0]:
         nI = nI + 1
         if i == str(1) and nI == 0:
@@ -101,6 +177,11 @@ def add_faceit(iElo, iRank, acEloToday, iStreak, iMatches, iMatchesWon):
             core.add_text("\tCurrent Elo")
             core.add_same_line(xoffset=130)
             core.add_text("elo##", default_value=f"{iElo}")
+        if acEloGoal and skipacEloGoal == 0:
+            core.add_text("\tElo Goal")
+            core.add_same_line(xoffset=130)
+            core.add_text("eloGoal##", default_value=f"{acEloGoal}")
+            skipacEloGoal = 1
         if i == str(1) and nI == 1:
             logging.info("Add Rank")
             core.add_text("\tRank")
@@ -126,6 +207,7 @@ def add_faceit(iElo, iRank, acEloToday, iStreak, iMatches, iMatchesWon):
             core.add_text("\tMatches Won")
             core.add_same_line(xoffset=130)
             core.add_text("matcheswon##", default_value=f"{iMatchesWon}")
+
 
 
 def add_last_game(acMap, acResult, acScore, acKd, acEloDiff, iKills, iDeath):
@@ -172,12 +254,19 @@ def add_last_game(acMap, acResult, acScore, acKd, acEloDiff, iKills, iDeath):
             core.add_text("death##", default_value=f"{iDeath}")
 
 
+def add_progress_bar():
+    with simple.group("##ProgressGroup"):
+        core.add_spacing(count=20)
+        core.add_text("##ProgressText", default_value="Getting data from Faceit servers")
+        core.add_progress_bar("##ProgressBar", default_value=0.0, overlay="f{nI}", width=180)
+
+
 def switch_back_to_menu():
     logging.info("Switch back to start menu")
     global start_threading
     start_threading = 0
     hwnd = win32gui.GetForegroundWindow()
-    win32gui.MoveWindow(hwnd, 0, 0, 492, 730, True)
+    win32gui.MoveWindow(hwnd, 0, 0, 492, 780, True)
     simple.show_item("##Overlay")
     simple.show_item("##Config")
     faceit_name = config_functions.get_faceit_name_from_db()
@@ -192,7 +281,7 @@ def switch_back_to_menu():
 
 def show_main():
     logging.info("start show_main")
-    global start_threading
+    global start_threading, on_render, on_progress, build_is_done
     heigh, iCountMatch, iCountFaceit = config_functions.check_for_layout()
     name = config_functions.get_faceit_name_from_db()
     with simple.window(f"{name} Elo", height=heigh, width=190,
@@ -205,28 +294,34 @@ def show_main():
         core.set_main_window_size(width=250, height=heigh)
         # Now the magic happens !
         core.set_style_frame_rounding(6.00)
+        add_progress_bar()
+        on_render = 1
         """
         Get Data from the API
         """
+        on_progress = 10
         logging.info("Get data from the API")
         iElo, acEloToday, iRank, \
         acResult, acScore, acKd, \
         acMap, iStreak, iMatches, \
         iMatchesWon, acEloDiff, iKills, \
         iDeath = faceit_api.get_faceit_data_from_api()
+        on_progress = 50
         """
         Build the Faceit Header and Data
         """
+        on_progress = 80
         if iCountFaceit > 0:
             logging.info("Build the window for Faceit stats")
             add_faceit(iElo, iRank, acEloToday, iStreak, iMatches, iMatchesWon)
         """
         Build the Last Game Header and Data
         """
+        on_progress = 90
         if iCountMatch > 0:
             logging.info("Build the window for Match stats")
             add_last_game(acMap, acResult, acScore, acKd, acEloDiff, iKills, iDeath)
-
+        on_progress = 100
         """
         ! Add some promotion !
         """
@@ -234,8 +329,11 @@ def show_main():
         core.add_text("powered by Dear PyGui")
         core.add_same_line()
         core.add_image("image##DPG", "resources/6727dpg.ico")
-
+        on_render = 0
+        build_is_done = 1
+        simple.hide_item("##ProgressGroup")
         core.enable_docking(dock_space=False)
         hwnd = win32gui.GetForegroundWindow()
         win32gui.SetWindowText(hwnd, f"{name} Elo")
         start_threading = 1
+
