e509c3ffb9858e2f61b808cdb4b7208b7f180b20
Illu <52736876+Mstpyt@users.noreply.github.com>
2021-04-17 16:45:11 +0200

    Update faceit_data.py

--- faceit/faceit_data.py
+++ faceit/faceit_data.py
@@ -1,8 +1,51 @@
-import json
-
-import requests
-import threading
 import logging
+import threading
+from time import sleep
+
+import ratelimit
+import requests
+from ratelimit import limits
+
+from config import FACEIT_API
+
+
+class RateLimitException(Exception):
+    pass
+
+
+@limits(calls=1000, period=60 * 60)  # Rate limit is 600/600 but let's play it safe
+def fetch_data(api_call):
+    faceitData = FaceitData(FACEIT_API)
+    try:
+        session = faceitData.get_session()
+        with session.get(api_call, headers=faceitData.headers) as response:
+            if response.status_code in [400, 403] and "invalid key" in response.text:  # Invalid API key
+                return "Invalid Key"
+            elif response.status_code == 200:
+                return response.json()
+            elif response.status_code == 429:  # Rate limit
+                raise RateLimitException()
+            raise requests.RequestException()  # API down
+    except requests.RequestException:
+        logging.exception("Failed to fetch API")
+        return None
+        pass
+
+
+def limit_fetch_data(api_data):
+    while True:
+        try:
+            return fetch_data(api_data)
+        except ratelimit.RateLimitException as exception:
+            logging.info(
+                "Ran into the soft API limit, waiting {} seconds.".format(
+                    exception.period_remaining
+                )
+            )
+            sleep(exception.period_remaining)
+        except RateLimitException:
+            logging.warning("Got rate-limited, waiting 5 minutes.")
+            sleep(60 * 5)
 
 
 class FaceitData:
@@ -31,7 +74,6 @@ class FaceitData:
             self.thread_local.session = requests.Session()
         return self.thread_local.session
 
-
     def match_stats(self, match_id=None):
         """Retrieve match details
 
@@ -43,12 +85,16 @@ class FaceitData:
             logging.info("match_id cannot be nothing")
         else:
             api_url = "{}/matches/{}/stats".format(self.base_url, match_id)
-            session = self.get_session()
+            result = limit_fetch_data(api_url)
+            return result
+        """
+           session = self.get_session()
             with session.get(api_url, headers=self.headers) as res:
                 if res.status_code is 200:
                     return json.loads(res.content.decode('utf-8'))
                 else:
                     return None
+                    """
 
     # Players
 
@@ -72,13 +118,8 @@ class FaceitData:
         if game is not None:
             api_url += "&game={}".format(game)
 
-        # print(api_url)
-        session = self.get_session()
-        with session.get(api_url, headers=self.headers) as res:
-            if res.status_code is 200:
-                return json.loads(res.content.decode('utf-8'))
-            else:
-                return None
+        result = limit_fetch_data(api_url)
+        return result
 
     def player_id_details(self, player_id=None):
         """Retrieve player details
@@ -90,13 +131,8 @@ class FaceitData:
             logging.info("The player_id cannot be nothing!")
         else:
             api_url = "{}/players/{}".format(self.base_url, player_id)
-
-            session = self.get_session()
-            with session.get(api_url, headers=self.headers) as res:
-                if res.status_code is 200:
-                    return json.loads(res.content.decode('utf-8'))
-                else:
-                    return None
+            result = limit_fetch_data(api_url)
+            return result
 
     def player_matches(self, player_id=None, game=None, from_timestamp=None, to_timestamp=None,
                        starting_item_position=0, return_items=1):
@@ -126,12 +162,8 @@ class FaceitData:
                 else:
                     api_url += "?from={}".format(from_timestamp)
 
-                session = self.get_session()
-                with session.get(api_url, headers=self.headers) as res:
-                    if res.status_code is 200:
-                        return json.loads(res.content.decode('utf-8'))
-                    else:
-                        return None
+                result = limit_fetch_data(api_url)
+                return result
 
     def player_stats(self, player_id=None, game_id=None):
         """Retrieve the statistics of a player
@@ -149,9 +181,5 @@ class FaceitData:
                 api_url = "{}/players/{}/stats/{}".format(
                     self.base_url, player_id, game_id)
 
-                session = self.get_session()
-                with session.get(api_url, headers=self.headers) as res:
-                    if res.status_code is 200:
-                        return json.loads(res.content.decode('utf-8'))
-                    else:
-                        return None
+                result = limit_fetch_data(api_url)
+                return result
